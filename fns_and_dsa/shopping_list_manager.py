e19ed31baba32f7870de60e83bab8a9290446868
Naledi Mokoena <125870439+Naledi84@users.noreply.github.com>
2025-09-23 12:10:04 +0200

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,1 +1,13 @@
-life
+if choice == '1':
+    item = input("Enter item to add: ")
+    shopping_list.append(item)
+elif choice == '2':
+    item = input("Enter item to remove: ")
+    if item in shopping_list:
+        shopping_list.remove(item)
+    else:
+        print("Item not found.")
+elif choice == '3':
+    print("Current List:")
+    for item in shopping_list:
+        print(f"- {item}")
