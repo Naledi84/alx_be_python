2ceca2809e1f9e47714991bcd2afd8ea765609cb
Naledi Mokoena <125870439+Naledi84@users.noreply.github.com>
2025-09-23 12:11:59 +0200

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,3 +1,13 @@
+import datetime
+
+def display_current_datetime():
+    now = datetime.datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+
+def calculate_future_date(days):
+    future = datetime.datetime.now() + datetime.timedelta(days=days)
+    return future.strftime("%Y-%m-%d")
+
 if choice == '1':
     item = input("Enter item to add: ")
     shopping_list.append(item)
