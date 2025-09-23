b0e8b93200f3ef81dad353ae46eee941a0d26655
Naledi Mokoena <125870439+Naledi84@users.noreply.github.com>
2025-09-23 12:04:07 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,13 @@
-
+def performace_operation(num1, num2, operation):
+  if operation == 'add':
+    return num1 + num2
+  elif operation == 'subtract':
+    return num1-num2
+  elif operation == 'multiply':
+    return num1 * num2
+  elif operation == 'divide'
+if num2 ==0 
+    return "Error: division by zero"
+    return num1/num2
+else:
+    return "Error:Inalid operation"
